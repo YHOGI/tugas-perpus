194d1f99af4e59c1d0e6e1cc065259abd3377947
YHOGI <164154524+YHOGI@users.noreply.github.com>
2024-03-20 16:28:41 +0700

    Add files via upload

---
+++ validation-login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perpustakaan</title>
+</head>
+<body>
+    <h1>
+       <br>SILAKAN LOGIN TERLEBIH DAHULU</br>
+        <a href="../login.php">Login</a>
+    </h1>
+</body>
+</html>
